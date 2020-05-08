d8233b1e77840f00899d56ed4a29dd153d285877
Palpatinep <panu.pattikangas@gmail.com>
2020-05-09 01:09:15 +0300

    setup

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const express = require("express");
 const app = express();
 const expresslayouts = require("express-ejs-layouts");
 
+const mongoose = require("mongoose");
+// connects to DATABSE_URL string in ./env/
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on("error", error => console.error(error));
+db.on("open", () => console.log("Connected to Mongoose"));
+
+
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/indexlayout");
@@ -16,6 +24,7 @@ const fifaRouter = require("./routes/fifarouter");
 const biljardiRouter = require("./routes/biljardirouter");
 
 app.use(expresslayouts);
+app.use(express.static, (__dirname + "/public");)
 app.use("/", indexRouter);
 app.use("/fifa", fifaRouter);
 app.use("/biljardi", biljardiRouter);
